f01749b5ae8fa0f816760c8fe5dcd54c6ccffd3e
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-08-10 15:39:08 -0500

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://incandescent-khapse-1a9ad1.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kenya Caines
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kenyacaines/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced.
+        </a>
+      </footer>
+    </div>
+  );
 }
