79e5276306f3b8bc7060ae98266b37441d84edbd
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-08-11 14:14:29 -0500

    Updated App CSS

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://incandescent-khapse-1a9ad1.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="www.kenyacaines.com"
-          >
-            Kenya Caines
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/kenyacaines/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub.
-          </a>
-        </footer>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://incandescent-khapse-1a9ad1.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="www.kenyacaines.com"
+        >
+          Kenya Caines
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kenyacaines/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
