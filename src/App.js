aab665e37f5e987faa986f8a8581781aa3db0b32
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-08-11 13:05:14 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://incandescent-khapse-1a9ad1.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          title="www.kenyacaines.com"
-        >
-          Kenya Caines
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kenyacaines/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://incandescent-khapse-1a9ad1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            title="www.kenyacaines.com"
+          >
+            Kenya Caines
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kenyacaines/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
