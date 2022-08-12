0266ce6a800f372a43d1d00d98ba76a7b788fc9d
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-08-12 16:58:09 -0500

    Added PM and AM to the time

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -16,13 +16,22 @@ export default function FormattedDate(props) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
+ 
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+   if (hours>11){
+    return (
+        <div>
+            {day} {hours}:{minutes} PM
+            </div>
+    );
+  }else{
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {hours}:{minutes} AM
     </div>
   );
+  }
 }
